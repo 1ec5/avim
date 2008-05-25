ced16a684bb31f0269deb88582e0f62226142dba
Minh Nguyễn <mxn@1ec5.org>
2008-05-25 21:06:20 +0000

    Laid groundwork for Thunderbird support; added in build.sh from Extension Wizard

---
+++ config_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Build config for build.sh
+APP_NAME=avim
+CHROME_PROVIDERS="chrome/content chrome/locale"
+CLEAN_UP=1
+ROOT_FILES="CHANGELOG LICENSE"
+ROOT_DIRS="defaults"
+PRUNE_DIRS="*CVS*"
+BEFORE_BUILD=
+AFTER_BUILD=
