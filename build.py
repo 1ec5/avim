f55f84449f93ea3e115ce845cf78f918ac6d98fd
Minh Nguyễn <mxn@1ec5.org>
2008-09-21 20:25:06 +0000

    Removed Bash build script; bumped Python build script version; factored build script configuration out into a separate script; began adding support for Selenium IDE

--- build.py
+++ build.py
@@ -6,9 +6,10 @@ Mozilla-based applications as an extension. Inspired by build.sh by Nickolay
 Ponomarev.
 
 This module requires Python 2.5 or above and is dependent on the following
-command-line utilities: python, svn, svnversion."""
+command-line utilities: python, svn, svnversion. It takes its configuration from
+config_build.py in the same directory as itself."""
 
-__version__ = "1.0"
+__version__ = "2.0"
 __authors__ = ["Minh Nguyen <mxn@1ec5.org>"]
 __license__ = """\
 Copyright (c) 2008 Minh Nguyen.
@@ -34,88 +35,16 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE."""
 
+import sys, subprocess, os, shutil, zipfile, re, hashlib, decimal
 from os import path
 from datetime import date
+from config_build import *
 
 class BuildConfig:
     """An enumeration of build configurations."""
     RELEASE = "Release"
     DEBUG = "Debug"
     L10N = "BabelZilla"
-
-### Configuration ##############################################################
-
-# True if the script should produce a testing build; false if it should produce
-# a release build.
-CONFIG = BuildConfig.RELEASE
-
-# Version number of the avim.js release used in this build. Included in the
-# extension's version string.
-AVIM_VERSION = 20080728
-
-# Revision number in the Subversion repository.
-REVISION = None
-
-# Build date.
-DATE = None
-
-# Name to use in the build's directories.
-PACKAGE_NAME = "avim"
-
-# Paths to directories that consitute the chrome JAR file.
-CHROME_PROVIDERS = ["content", "locale", "skin"]
-
-# Paths to miscellaneous files that should be included in the build's root
-# directory. install.rdf and chrome.manifest are automatically included.
-ROOT_FILES = ["LICENSE"]
-
-# Paths to directories that should be included, uncompressed, in the build's
-# root directory.
-ROOT_DIRS = ["defaults"]
-
-# Paths to files to be preprocessed. These files contain placeholders that
-# should be interpreted as variables.
-VAR_FILES = ["install.rdf", "chrome.manifest", "CHANGELOG", "LICENSE",
-             path.join("content", "options.js")]
-
-# Names of files to be preprocessed.
-VAR_NAMES = ["options.properties"]
-
-# Paths to directories that should be omitted from a release build.
-DEBUG_DIRS = [path.join("content", "test"), path.join("skin", "test")]
-
-# Dictionary mapping subdirectories of locale/ to BabelZilla-compatible locale
-# codes. Locale names that are already compatible can be omitted.
-LOCALE_DIRS = {"es": "es-ES", "vi": "vi-VN"}
-
-# Paths to files to which a license block will be prepended.
-LICENSE_FILES = [path.join("content", "avim.js")]
-
-# A dictionary of language-specific comment specifications in the form: (block
-# start, line start, block end).
-COMMENT_FORMATS = {"C": ("/*", " * ", " */"),
-                   "Python": ('__license__ = """\\', None, '"""'),
-                   "SGML": ("<!--", "\t", "  -->"),
-                   "shell": (None, "# ", None)}
-
-# A dictionary mapping lowercase file extensions to the comment format of the
-# file's language.
-EXT_COMMENTS = {"c": "C", "cpp": "C", "h": "C", "m": "C", "java": "C", "js": "C",
-                "jsm": "C", "php": "C", "css": "C", "py": "Python",
-                "html": "SGML", "xhtml": "SGML", "xml": "SGML", "rdf": "SGML",
-                "xul": "SGML", "dtd": "SGML", "xsl": "SGML", "xslt": "SGML",
-                "svg": "SGML", "mml": "SGML", "pl": "shell", "rb": "shell",
-                "manifest": "shell", "properties": "shell", "sh": "shell"}
-
-# A dictionary mapping lowercase file extensions to the beginnings of lines
-# that must appear at the beginning of a file (prolog or shebang lines).
-EXT_PROLOGS = {"py": "#!", "xhtml": "<?xml", "xml": "<?xml", "rdf": "<?xml",
-               "xul": "<?xml", "xsl": "<?xml", "xslt": "<?xml", "svg": "<?xml",
-               "mml": "<?xml", "pl": "#!", "rb": "#!", "sh": "#!"}
-
-################################################################################
-
-import sys, subprocess, os, shutil, zipfile, re, hashlib, decimal
 
 def print_help(version):
     """Prints help information to the command line."""
---
+++ config_build.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+"""AVIM build configuration"""
+
+from os import path
+from datetime import date
+from build import BuildConfig
+
+# True if the script should produce a testing build; false if it should produce
+# a release build.
+CONFIG = BuildConfig.RELEASE
+
+# Version number of the avim.js release used in this build. Included in the
+# extension's version string.
+AVIM_VERSION = 20080728
+
+# Revision number in the Subversion repository.
+REVISION = None
+
+# Build date.
+DATE = None
+
+# Name to use in the build's directories.
+PACKAGE_NAME = "avim"
+
+# Paths to directories that consitute the chrome JAR file.
+CHROME_PROVIDERS = ["content", "locale", "skin"]
+
+# Paths to miscellaneous files that should be included in the build's root
+# directory. install.rdf and chrome.manifest are automatically included.
+ROOT_FILES = ["LICENSE"]
+
+# Paths to directories that should be included, uncompressed, in the build's
+# root directory.
+ROOT_DIRS = ["defaults"]
+
+# Paths to files to be preprocessed. These files contain placeholders that
+# should be interpreted as variables.
+VAR_FILES = ["install.rdf", "chrome.manifest", "CHANGELOG", "LICENSE",
+             path.join("content", "options.js")]
+
+# Names of files to be preprocessed.
+VAR_NAMES = ["options.properties"]
+
+# Paths to directories that should be omitted from a release build.
+DEBUG_DIRS = [path.join("content", "test"), path.join("skin", "test")]
+
+# Dictionary mapping subdirectories of locale/ to BabelZilla-compatible locale
+# codes. Locale names that are already compatible can be omitted.
+LOCALE_DIRS = {"es": "es-ES", "vi": "vi-VN"}
+
+# Paths to files to which a license block will be prepended.
+LICENSE_FILES = [path.join("content", "avim.js")]
+
+# A dictionary of language-specific comment specifications in the form: (block
+# start, line start, block end).
+COMMENT_FORMATS = {"C": ("/*", " * ", " */"),
+                   "Python": ('__license__ = """\\', None, '"""'),
+                   "SGML": ("<!--", "\t", "  -->"),
+                   "shell": (None, "# ", None)}
+
+# A dictionary mapping lowercase file extensions to the comment format of the
+# file's language.
+EXT_COMMENTS = {"c": "C", "cpp": "C", "h": "C", "m": "C", "java": "C",
+                "js": "C", "jsm": "C", "php": "C", "css": "C", "py": "Python",
+                "html": "SGML", "xhtml": "SGML", "xml": "SGML", "rdf": "SGML",
+                "xul": "SGML", "dtd": "SGML", "xsl": "SGML", "xslt": "SGML",
+                "svg": "SGML", "mml": "SGML", "pl": "shell", "rb": "shell",
+                "manifest": "shell", "properties": "shell", "sh": "shell"}
+
+# A dictionary mapping lowercase file extensions to the beginnings of lines
+# that must appear at the beginning of a file (prolog or shebang lines).
+EXT_PROLOGS = {"py": "#!", "xhtml": "<?xml", "xml": "<?xml", "rdf": "<?xml",
+               "xul": "<?xml", "xsl": "<?xml", "xslt": "<?xml", "svg": "<?xml",
+               "mml": "<?xml", "pl": "#!", "rb": "#!", "sh": "#!"}
