56ac640626f6cd4614c2e76f662df1e6b2f49311
Minh Nguyễn <mxn@1ec5.org>
2010-03-08 01:10:33 +0000

    Released version 20080728.366 and prepared for next release Added missing stylesheet (no effect) Added ability for Script Monitor to disable VietUni 1.7 Updated TODO

--- config_build.py
+++ config_build.py
@@ -18,10 +18,10 @@ REPO_URL = "http://version.1ec5.org/viewvc/avim/trunk/%(path)s" \
            "?revision=%(rev)s&view=markup"
 
 # Revision number in the Subversion repository.
-REVISION = None
+REVISION = 366
 
 # Build date.
-DATE = None
+DATE = date(2010, 3, 7)
 
 # Name to use in the build's directories.
 PACKAGE_NAME = "avim"
