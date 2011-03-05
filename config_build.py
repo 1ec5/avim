6ee0538dc690eff73eff7ef4e0e6d78cd91cdde7
Minh Nguyễn <mxn@1ec5.org>
2011-03-05 11:09:41 +0000

    Released version 20080728.450 Prepared for next release

--- config_build.py
+++ config_build.py
@@ -18,10 +18,10 @@ REPO_URL = "http://version.1ec5.org/viewvc/avim/trunk/%(path)s" \
            "?revision=%(rev)s&view=markup"
 
 # Revision number in the Subversion repository.
-REVISION = None
+REVISION = 450
 
 # Build date.
-DATE = None
+DATE = date(2011, 3, 5)
 
 # Name to use in the build's directories.
 PACKAGE_NAME = "avim"
