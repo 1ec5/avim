1ec5/avim

commit 8c8b02fff9219e5e2480b42d6274d63809422126

Don’t send input event to iCloud input fields
Fixes #36.

content/avim.js
@@ -1836,6 +1836,7 @@ function AVIM()	{
 	 */
 	this.updateContainer = function(outer, inner) {
 		if (!inner) return;
+		if (inner.document.location.hostname === iCloudHostname) return; // #36
 		var inputEvent = document.createEvent("Events");
 		inputEvent.initEvent("input", true, true);
 		if (inner.dispatchEvent) inner.dispatchEvent(inputEvent);

commit bca57f83b6ee95a4f156242239635d1cb648e895

Fixed JavaScript error while typing in chrome
Regression from 8c8b02f for #36.

content/avim.js
@@ -1836,7 +1836,7 @@ function AVIM()	{
 	 */
 	this.updateContainer = function(outer, inner) {
 		if (!inner) return;
-		if (inner.document.location.hostname === iCloudHostname) return; // #36
+		if (inner.document && inner.document.location.hostname === iCloudHostname) return; // #36
 		var inputEvent = document.createEvent("Events");
 		inputEvent.initEvent("input", true, true);
 		if (inner.dispatchEvent) inner.dispatchEvent(inputEvent);